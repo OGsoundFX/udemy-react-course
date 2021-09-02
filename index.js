b67b91005dbf4c8db447bd1c17ffbfd75ca9538f
Olivier Girardot <olivier.girardot@axonetiq.com>
2021-09-02 21:22:13 +0200

    small progress

--- index.js
+++ index.js
@@ -9,14 +9,14 @@ class Hello extends React.Component {
   }
 }
 
-// function Goodbye() {
-//   return (
-//     <div>
-//       <h1>Goodbye!</h1>
-//       <p>This was nice</p>
-//     </div>
-//   );
-// }
+function Goodbye() {
+  return (
+    <div>
+      <h1>Goodbye!</h1>
+      <p>This was nice</p>
+    </div>
+  );
+}
 
 ReactDOM.render(<Hello/>, document.getElementById('root'));
-// ReactDOM.render(<Goodbye/>, document.getElementById('root'));
+ReactDOM.render(<Goodbye/>, document.getElementById('goodbye'));
